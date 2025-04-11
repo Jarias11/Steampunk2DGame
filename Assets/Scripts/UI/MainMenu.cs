8489433b9a6ea13d7eacfc4a9fd94f36b3aa5250
Jeremy Arias <jeremyraph@msn.com>
2025-04-10 20:12:43 -0400

    created a start menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu: MonoBehaviour
+{
+    public void StartGame(){
+        SceneManager.LoadScene("Main Town");
+    }
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Game Quit (only works in build)");
+    }
+}
