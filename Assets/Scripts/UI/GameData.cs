f9a56b317540cf1f3abe976fda2b8c4b74b56ab9
Thandar Tun <ttun6@gatech.edu>
2025-05-05 22:07:08 -0400

    Continue button
    -Temp save file added

---
+++ Assets/Scripts/UI/GameData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO; 
+
+public class GameData
+{
+    public string playerName;
+    public int level;
+    public float positionX;
+    public float positionY;
+    public float positionZ;
+    public string savedScene; // Add this
+
+}
+
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
+
 
 public class MainMenu : MonoBehaviour {
+    public string levelToLoad;
+    [SerializeField] private GameObject noSavedGame = null;
     public void StartGame() {
+        CreateSaveFile(); // Create dummy save data
         SceneManager.LoadScene("Main Town");
     }
+
+    public void LoadGame(){
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            GameData data = JsonUtility.FromJson<GameData>(json);
+
+            Debug.Log("Continuing from scene: " + data.savedScene);
+            SceneManager.LoadScene(data.savedScene); // Load the saved scene
+        }
+        else
+        {
+            noSavedGame.SetActive(true); // Show error UI if no save exists
+        }
+    }
+
+    public void CreateSaveFile(){
+        GameData data = new GameData();
+        data.playerName = "Player1";
+        data.level = 1;
+        data.positionX = 0;
+        data.positionY = 0;
+        data.positionZ = 0;
+        data.savedScene = "Main Town"; //Save 
+
+        string json = JsonUtility.ToJson(data);
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        File.WriteAllText(path, json);
+        Debug.Log("Save file created at: " + path);
+        }
+
     public void QuitGame() {
         Application.Quit();
         Debug.Log("Game Quit (only works in build)");
